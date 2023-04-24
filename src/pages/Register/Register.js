1e58d49fcbdc38225aa272b7bfde9af9e6426f12
Wilian Silva de Oliveira <wilian.oliveira@credibrf.com.br>
2023-04-24 15:48:20 -0300

    register: form

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,10 +1,51 @@
+import "./Register.css";
+
 const Register = () => {
     return(
-        <>
+        <div className="register">
             <h2>
                 Register
             </h2>
-        </>
+            <form>
+                <label>
+                    <span>Nome: </span>
+                    <input 
+                        type="text"
+                        name="displayName"
+                        required
+                        placeholder="Nome do usuário"
+                    />
+                </label>
+                <label>
+                    <span>E-mail: </span>
+                    <input 
+                        type="email"
+                        name="email"
+                        required
+                        placeholder="E-mail do usuário"
+                    />
+                </label>
+                <label>
+                    <span>Senha: </span>
+                    <input 
+                        type="password"
+                        name="password"
+                        required
+                        placeholder="Insira sua senha"
+                    />
+                </label>
+                <label>
+                    <span>Confirme sua senha: </span>
+                    <input 
+                        type="password"
+                        name="confirmPassword"
+                        required
+                        placeholder="Confirme sua senha"
+                    />
+                </label>
+                <button className="btn">Registrar-se</button>
+            </form>
+        </div>
     )
 }
 export default Register;
