254bb987258be232a7739543e23c07f0ff9f3825
Wilian Silva de Oliveira <wilian.oliveira@credibrf.com.br>
2023-04-25 17:06:12 -0300

    hook: useAuthentication

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Register.css";
+import { useAuthentication } from "../../hook/useAuthentication";
 
 const Register = () => {
     const [displayName, setDisplayName] = useState("");
@@ -8,7 +9,9 @@ const Register = () => {
     const [confirmPassword, setConfirmPassword] = useState("");
     const [error, setError] = useState("");
 
-    const handleSubmit= (e) => {
+    const {createUser, error: authError, loading} = useAuthentication();
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         setError("");
@@ -24,8 +27,16 @@ const Register = () => {
             return
         }
 
-        console.log(user)
+        const res = await createUser(user);
+
+        console.log(res)
     }
+
+    useEffect(() => {
+        if(authError) {
+            setError(authError);
+        }
+    }, [authError])
 
     return(
         <div className="register">
@@ -78,7 +89,13 @@ const Register = () => {
                         onChange={(e) => setConfirmPassword(e.target.value)}
                     />
                 </label>
-                <button className="btn">Registrar-se</button>
+                {!loading && <button className="btn">Registrar-se</button>}
+                {loading && (
+                        <button className="btn" disabled>
+                            Aguarde...
+                        </button>
+                    )}
+                {error && <p className="error">{error}</p>}
             </form>
         </div>
     )
