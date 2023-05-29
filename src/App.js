816be6f7cfe566a36298a84fbddc557be0d7e76b
Wilian Silva de Oliveira <wilian.oliveira@credibrf.com.br>
2023-05-29 15:18:19 -0300

    bloqueio de páginas usuário não autênticado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 // autenticação
 import { onAuthStateChanged } from 'firebase/auth';
@@ -49,11 +49,23 @@ function App() {
             <div className='container'>
               <Routes>
                 <Route exact path="/" element={<Home/>}/>
-                <Route path="/login" element={<Login/>} />
-                <Route path="/register" element={<Register/>} />
+                <Route
+                  path="/login"
+                  element={!user ? <Login/> : <Navigate to="/" />}
+                />
+                <Route
+                  path="/register"
+                  element={!user ? <Register/> : <Navigate to="/" />}
+                />
+                <Route
+                  path="/posts/create"
+                  element={user ? <CreatePost/> : <Navigate to="/login" />}
+                />
+                <Route
+                  path="/dashboard"
+                  element={user ? <Dashboard/> : <Navigate to="/login" />}
+                />
                 <Route path="/about" element={<About/>}/>
-                <Route path="/posts/create" element={<CreatePost/>} />
-                <Route path="/dashboard" element={<Dashboard/>} />
               </Routes>
             </div>
           <Footer/>
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,18 @@
+import styles from "./About.module.css";
+
+import { Link } from "react-router-dom";
+
 const About = () => {
     return(
-        <>
+        <div className={styles.about}>
             <h2>
-                About
+                Sobre o mini <span>Blog</span>
             </h2>
-        </>
+            <p>
+                Este projeto consiste em um blog feito com React no Front-End e Firebase no back-end.
+            </p>
+            <Link to="/post" className="btn">Criar post</Link>
+        </div>
     )
 }
 export default About;
